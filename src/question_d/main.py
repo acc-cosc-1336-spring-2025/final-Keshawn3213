62f86ae5a6a0756d950187cde4c0d9694cadc35f
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-17 13:20:14 -0500

    Update main.py

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,10 @@
-#add import
+def get_most_likely_ancestor_consensus(dna_string1, dna_string2):
+    positions = []
+    len_s1 = len(dna_string1)
+    len_s2 = len(dna_string2)
+
+    for i in range(len_s1 - len_s2 + 1):
+        if dna_string1[i:i + len_s2] == dna_string2:
+            positions.append(i + 1)  # +1 for 1-based index
+
+    return tuple(positions)
