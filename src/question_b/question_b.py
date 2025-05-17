40992263a14f45aec43e7954ff0e78b084691d85
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-17 13:06:19 -0500

    Update question_b.py

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,36 +1,3 @@
-class Stock:
-    def __init__(self, symbol, company_name):
-        self.__symbol = symbol
-        self.__company_name = company_name
-
-    def get_symbol(self):
-        return self.__symbol
-
-    def get_company_name(self):
-        return self.__company_name
-
-
-def get_stock_list():
-    stock1 = Stock("AAPL", "Apple Inc")
-    stock2 = Stock("CAT", "Caterpillar")
-    stock3 = Stock("EK", "Eastman Kodak")
-    stock4 = Stock("GOOG", "Google")
-    stock5 = Stock("MSFT", "Microsoft")
-    return [stock1, stock2, stock3, stock4, stock5]
-
-
-def display_stock_list(stock_list):
-    print("\nStock Report")
-    print(f"{'Company':<20} {'Symbol'}")
-    print("-" * 30)
-    for stock in stock_list:
-        print(f"{stock.get_company_name():<20} {stock.get_symbol()}")
-    print()
-
-
-def main():
-    stock_list = get_stock_list()
-
     while True:
         print("Menu:")
         print("1 - Display stock purchase history")
