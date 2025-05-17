330f5ed4956ac4cd668613bb2dd1a495656213aa
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-17 13:08:05 -0500

    Update question_c.py

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,19 @@
-#write functions here, don't add input('') statements here!
+def main():
+    while True:
+        print("Menu:")
+        print("1 - Display stock purchase history")
+        print("2 - Exit")
+        choice = input("Enter your choice: ").strip()
+
+        if choice == '1':
+            stock_list = get_stock_list()
+            display_stock_list(stock_list)
+        elif choice == '2':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option. Please try again.\n")
+
+
+if __name__ == "__main__":
+    main()
