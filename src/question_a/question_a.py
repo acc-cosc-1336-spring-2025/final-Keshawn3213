8e19fdfbc845a8a3f311ae42e12532cb88c5b4a9
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-17 13:03:47 -0500

    Update question_a.py

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,13 @@
-#write functions here, don't add input('') statements here!
-def test_config():
-    return True
+def main():
+    while True:
+        table = create_multiplication_table()
+        display_multiplication_table(table)
+
+        choice = input("Do you want to generate the table again? (yes/no): ").strip().lower()
+        if choice not in ("yes", "y"):
+            print("Goodbye!")
+            break
+
+
+if __name__ == "__main__":
+    main()
